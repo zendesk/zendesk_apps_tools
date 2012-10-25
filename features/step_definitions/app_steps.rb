5a7a5e792e33b078f519b1ce534b396a04a633e1
James A. Rosen and Kenshiro Nakagawa <dev+james+knakagawa@zendesk.com>
2012-10-25 15:04:36 -0700

    app steps: set Aruba working dir to created app dir

--- features/step_definitions/app_steps.rb
+++ features/step_definitions/app_steps.rb
@@ -7,5 +7,7 @@ Given /^an app directory$/ do
   @app_dir = Pathname.new( Dir.tmpdir ).join("app_#{r}").tap do |path|
     FileUtils.mkdir_p path
   end
+  # Set the Aruba temp dir to the app dir:
+  @dirs = [ @app_dir ]
   raise "Could not create app dir" unless Dir.exists?(@app_dir.to_s)
 end
