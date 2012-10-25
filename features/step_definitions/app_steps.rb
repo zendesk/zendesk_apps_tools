2358132dfe23a37342790b9e7ea51eec6821bbc1
James A. Rosen and Kenshiro Nakagawa <dev+james+knakagawa@zendesk.com>
2012-10-25 15:05:18 -0700

    add "Given the file..." step definition

--- features/step_definitions/app_steps.rb
+++ features/step_definitions/app_steps.rb
@@ -11,3 +11,12 @@ Given /^an app directory$/ do
   @dirs = [ @app_dir ]
   raise "Could not create app dir" unless Dir.exists?(@app_dir.to_s)
 end
+
+Given /^the file "(.*?)" with:$/ do |filename, contents|
+  raise "No app dir exists. You forgot the 'Given an app directory' step" unless @app_dir
+  filename = @app_dir.join(filename)
+  FileUtils.mkdir_p File.dirname(filename)
+  File.open(filename, 'w') do |f|
+    f << contents
+  end
+end
