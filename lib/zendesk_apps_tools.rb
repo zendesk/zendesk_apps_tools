477bac7d2d3f19d35a55c168587e4a9019ff901d
James A. Rosen <james@zendesk.com>
2012-10-26 13:16:17 -0700

    extract ZendeskAppsTools::I18n from ValidationError

--- lib/zendesk_apps_tools.rb
+++ lib/zendesk_apps_tools.rb
@@ -2,6 +2,7 @@ module ZendeskAppsTools
   autoload :Command,    'zendesk_apps_tools/command'
   autoload :ZamFile,    'zendesk_apps_tools/zam_file'
   autoload :Connection, 'zendesk_apps_tools/connection'
+  autoload :I18n,       'zendesk_apps_tools/i18n'
   autoload :Package,    'zendesk_apps_tools/package'
 
   module Validations
