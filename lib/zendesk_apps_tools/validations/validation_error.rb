2b1a596f95b35a8caa0e827d731b447583ceeac1
James A. Rosen <james@zendesk.com>
2012-10-26 13:16:17 -0700

    extract ZendeskAppsTools::I18n from ValidationError

--- lib/zendesk_apps_tools/validations/validation_error.rb
+++ lib/zendesk_apps_tools/validations/validation_error.rb
@@ -2,26 +2,6 @@ module ZendeskAppsTools
   module Validations
 
     class ValidationError
-      class << self
-        attr_accessor :i18n_key_prefix
-
-        def i18n
-          @i18n ||= begin
-            require 'i18n'
-            ::I18n.load_path += locale_files
-            ::I18n
-          end
-        end
-
-        private
-
-        def locale_files
-          Dir[ File.expand_path('../../../../config/locales/*.yml', __FILE__) ]
-        end
-      end
-
-      self.i18n_key_prefix = 'zendesk_apps_tools.errors.'
-
       attr_reader :key, :data
 
       def initialize(key, data = nil)
@@ -29,7 +9,7 @@ module ZendeskAppsTools
       end
 
       def to_s
-        self.class.i18n.t("#{self.class.i18n_key_prefix}#{key}", data)
+        ZendeskAppsTools::I18n.t(key.to_s, data)
       end
     end
 
