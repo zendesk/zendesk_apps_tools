3167e0465053a6404e0f52e57b21625a6ad1d751
James A. Rosen <james@zendesk.com>
2012-10-26 16:45:54 -0700

    Package: declare inside "module ZendeskAppsTools"
    This lets us refer to other things in the same package without the
    ZendeskAppsTools:: prefix.

--- lib/zendesk_apps_tools/package.rb
+++ lib/zendesk_apps_tools/package.rb
@@ -2,100 +2,102 @@ require 'pathname'
 require 'multi_json'
 require 'jshintrb'
 
-class ZendeskAppsTools::Package
+module ZendeskAppsTools
+  class Package
 
-  def initialize(dir)
-    @dir = Pathname.new(File.expand_path(dir))
-  end
-
-  def root
-    @dir
-  end
-
-  def files
-    @files ||= Dir[ @dir.join('**/**') ].each_with_object([]) do |f, files|
-      relative_file_name = f.sub(/#{@dir}\/?/, '')
-      next unless File.file?(f)
-      next if relative_file_name =~ /^tmp\//
-      files << ZendeskAppsTools::AppFile.new(self, relative_file_name)
+    def initialize(dir)
+      @dir = Pathname.new(File.expand_path(dir))
     end
-  end
 
-  def template_files
-    @template_files ||= files.select { |f| f =~ /^templates\/.*\.hdbs$/ }
-  end
+    def root
+      @dir
+    end
 
-  def translation_files
-    @translation_files ||= files.select { |f| f =~ /^translations\// }
-  end
-
-  def validate
-    ZendeskAppsTools::Validations::Manifest.call(self) +
-      ZendeskAppsTools::Validations::Source.call(self) +
-      ZendeskAppsTools::Validations::Templates.call(self) +
-      ZendeskAppsTools::Validations::Translations.call(self)
-  end
-
-  def templates
-    @templates ||= begin
-      Dir[ @dir.join('templates/*.hdbs') ].inject({}) do |h, file|
-        str = File.read(file)
-        str.chomp!
-        h[File.basename(file, File.extname(file))] = str
-        h
+    def files
+      @files ||= Dir[ @dir.join('**/**') ].each_with_object([]) do |f, files|
+        relative_file_name = f.sub(/#{@dir}\/?/, '')
+        next unless File.file?(f)
+        next if relative_file_name =~ /^tmp\//
+        files << AppFile.new(self, relative_file_name)
       end
     end
-  end
 
-  def translations
-    @translations ||= begin
-      translation_dir = @dir.join('translations')
-      default_translations = MultiJson.load(File.read( translation_dir.join("#{default_locale}.json") ))
+    def template_files
+      @template_files ||= files.select { |f| f =~ /^templates\/.*\.hdbs$/ }
+    end
 
-      Dir[ translation_dir.join('*.json') ].inject({}) do |h, tr|
-        locale = File.basename(tr, File.extname(tr))
-        locale_translations = if locale == self.default_locale
-                                default_translations
-                              else
-                                default_translations.deep_merge(MultiJson.load(File.read(tr)))
-                              end
+    def translation_files
+      @translation_files ||= files.select { |f| f =~ /^translations\// }
+    end
 
-        h[locale] = locale_translations
-        h
+    def validate
+      Validations::Manifest.call(self) +
+        Validations::Source.call(self) +
+        Validations::Templates.call(self) +
+        Validations::Translations.call(self)
+    end
+
+    def templates
+      @templates ||= begin
+        Dir[ @dir.join('templates/*.hdbs') ].inject({}) do |h, file|
+          str = File.read(file)
+          str.chomp!
+          h[File.basename(file, File.extname(file))] = str
+          h
+        end
       end
     end
-  end
 
-  def locales
-    translations.keys
-  end
+    def translations
+      @translations ||= begin
+        translation_dir = @dir.join('translations')
+        default_translations = MultiJson.load(File.read( translation_dir.join("#{default_locale}.json") ))
 
-  def default_locale
-    manifest["default_locale"]
-  end
+        Dir[ translation_dir.join('*.json') ].inject({}) do |h, tr|
+          locale = File.basename(tr, File.extname(tr))
+          locale_translations = if locale == self.default_locale
+                                  default_translations
+                                else
+                                  default_translations.deep_merge(MultiJson.load(File.read(tr)))
+                                end
 
-  def translation(en)
-    translations[en]
-  end
+          h[locale] = locale_translations
+          h
+        end
+      end
+    end
 
-  def name
-    manifest["name"] || 'app'
-  end
+    def locales
+      translations.keys
+    end
 
-  def author
-    {
-      :name  => manifest['author']['name'],
-      :email => manifest['author']['email']
-    }
-  end
+    def default_locale
+      manifest["default_locale"]
+    end
 
-  def manifest
-    @manifest ||= begin
-      manifest_file = files.find { |f| f.relative_path == 'manifest.json' }
-      begin
-        MultiJson.load(manifest_file.read)
-      rescue Errno::ENOENT, Errno::EACCES, MultiJson::DecodeError
-        {}
+    def translation(en)
+      translations[en]
+    end
+
+    def name
+      manifest["name"] || 'app'
+    end
+
+    def author
+      {
+        :name  => manifest['author']['name'],
+        :email => manifest['author']['email']
+      }
+    end
+
+    def manifest
+      @manifest ||= begin
+        manifest_file = files.find { |f| f.relative_path == 'manifest.json' }
+        begin
+          MultiJson.load(manifest_file.read)
+        rescue Errno::ENOENT, Errno::EACCES, MultiJson::DecodeError
+          {}
+        end
       end
     end
   end
