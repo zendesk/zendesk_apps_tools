f4b53f9da03efaf75ae60c1534a901d8d5a7c95d
James A. Rosen <james@zendesk.com>
2012-10-26 17:10:55 -0700

    Package: move file initialization into #initialize

--- lib/zendesk_apps_tools/package.rb
+++ lib/zendesk_apps_tools/package.rb
@@ -5,27 +5,13 @@ require 'jshintrb'
 module ZendeskAppsTools
   class Package
 
-    attr_reader :root
+    attr_reader :root, :files, :template_files, :translation_files
 
     def initialize(dir)
       @root = Pathname.new(File.expand_path(dir))
-    end
-
-    def files
-      @files ||= Dir[ root.join('**/**') ].each_with_object([]) do |f, files|
-        relative_file_name = f.sub(/#{root}\/?/, '')
-        next unless File.file?(f)
-        next if relative_file_name =~ /^tmp\//
-        files << AppFile.new(self, relative_file_name)
-      end
-    end
-
-    def template_files
-      @template_files ||= files.select { |f| f =~ /^templates\/.*\.hdbs$/ }
-    end
-
-    def translation_files
-      @translation_files ||= files.select { |f| f =~ /^translations\// }
+      @files = non_tmp_files
+      @template_files = files.select { |f| f =~ /^templates\/.*\.hdbs$/ }
+      @translation_files = files.select { |f| f =~ /^translations\// }
     end
 
     def validate
@@ -33,6 +19,17 @@ module ZendeskAppsTools
         Validations::Source.call(self) +
         Validations::Templates.call(self) +
         Validations::Translations.call(self)
+    end
+
+    private
+
+    def non_tmp_files
+      Dir[ root.join('**/**') ].each_with_object([]) do |f, files|
+        relative_file_name = f.sub(/#{root}\/?/, '')
+        next unless File.file?(f)
+        next if relative_file_name =~ /^tmp\//
+        files << AppFile.new(self, relative_file_name)
+      end
     end
   end
 end
