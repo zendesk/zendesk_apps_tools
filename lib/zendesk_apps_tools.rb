dd0ada214a23e1532e504dae07535241bdc57309
Matthew Werner <mwerner@zendesk.com>
2012-11-20 12:07:58 -0800

    move connection back into apps_tools

--- lib/zendesk_apps_tools.rb
+++ lib/zendesk_apps_tools.rb
@@ -1,4 +1,5 @@
 module ZendeskAppsTools
-  autoload :Command,    'zendesk_apps_tools/command'
+  autoload :Command,      'zendesk_apps_tools/command'
+  autoload :Connection,   'zendesk_apps_tools/connection'
 end
 
