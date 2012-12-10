a0a759657d592d94f9c3ffb28a77cd7bdcc88d74
Likun Liu <lliu@zendesk.com>
2012-12-10 15:11:55 +1100

    moved the js generation to the support gem.

--- lib/zendesk_apps_tools/server.rb
+++ lib/zendesk_apps_tools/server.rb
@@ -1,6 +1,4 @@
 require 'sinatra/base'
-require 'erb'
-require 'json'
 require 'zendesk_apps_support/package'
 
 class Server < Sinatra::Base
@@ -8,24 +6,8 @@ class Server < Sinatra::Base
   set :port, ZendeskAppsTools::Command.port
 
   get '/app.js' do
-    manifest = JSON.parse(File.read("#{ZendeskAppsTools::Command.path}/app/manifest.json"))
-    @source = File.read("#{ZendeskAppsTools::Command.path}/app/app.js")
-    @name = manifest["name"]
-    @location = manifest["location"]
-    @asset_url_prefix = "http://localhost:#{ZendeskAppsTools::Command.port}"
-    @app_class_name = "app-0"
-    @author = manifest["author"]
-    @translations = {"app" => {}}
-    @framework_version = manifest["frameworkVersion"]
-
-    package = ZendeskAppsSupport::Package.new(ZendeskAppsTools::Command.path + "/app")
-    @templates = package.compiled_templates(0, "http://localhost:#{ZendeskAppsTools::Command.port}")
-
-    @settings = {}
-    manifest["parameters"].select {|param| param["default"]} .inject(@settings) {|hash, element| hash[element["name"]] = element["default"]}
-    @settings["title"] = @name
-
-    erb 'src.js'.to_sym, :content_type => 'text/javascript'
+    content_type 'text/javascript'
+    ZendeskAppsSupport::Package.new(ZendeskAppsTools::Command.path + "/app").readified_js(nil, 0, "http://localhost:#{ZendeskAppsTools::Command.port}")
   end
 
   get '*.png' do
