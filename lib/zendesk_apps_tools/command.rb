ac7bf342dd2ef28a2fa1533c7f5f03570a2a23a1
Pierre Nespo <pierre.nespo@gmail.com>
2013-01-29 11:08:16 +0100

    Revert "Do not take the <footer> mandatory in templates/layout.hdbs"
    This reverts commit 9dea39eb19dd5ace4713501ce8e448075db01866.

--- lib/zendesk_apps_tools/command.rb
+++ lib/zendesk_apps_tools/command.rb
@@ -17,10 +17,7 @@ module ZendeskAppsTools
     source_root File.expand_path(File.join(File.dirname(__FILE__), "../.."))
 
     desc "new", "Generate a new app"
-    method_option :footer, :type => :boolean, :default => true
     def new
-      @footer = options[:footer]
-
       puts "Enter this app author's name:"
       @author_name = get_value_from_stdin(/^\w.*$/, "Invalid name, try again:")
 
@@ -182,3 +179,4 @@ module ZendeskAppsTools
     end
   end
 end
+
