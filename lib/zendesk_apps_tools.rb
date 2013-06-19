c3d139534886e66e92c04db4287cdbde5985c926
Sean Caffery <scaffery@zendesk.com>
2013-06-19 15:12:02 +1000

    Match locale name generation to market place

--- lib/zendesk_apps_tools.rb
+++ lib/zendesk_apps_tools.rb
@@ -1,5 +1,6 @@
 module ZendeskAppsTools
   autoload :Command,      'zendesk_apps_tools/command'
   autoload :Translate,    'zendesk_apps_tools/translate'
+  autoload :LocaleIdentifier, 'zendesk_apps_tools/locale_identifier'
 end
 
