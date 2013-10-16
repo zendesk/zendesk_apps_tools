e9eab1f05d1918ae42eca6e585122237dafea5ab
Daniel Ribeiro <djbreves@gmail.com>
2013-10-17 10:14:51 +1100

    Update LocaleIdentifier comment

--- lib/zendesk_apps_tools/locale_identifier.rb
+++ lib/zendesk_apps_tools/locale_identifier.rb
@@ -2,7 +2,7 @@ module ZendeskAppsTools
   class LocaleIdentifier
     attr_reader :language_id
 
-    # Convert :"en-US-x-12" to 'en'
+    # Convert :"en-US-x-12" to 'en-US'
     def initialize(code)
       @language_id = code.sub(/-x-.*/, '')
     end
