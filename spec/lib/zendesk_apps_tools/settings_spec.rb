1946fc45eb648f2e2b170b57c9108b7e2f0d282b
Bulkan Evcimen <bulkan@gmail.com>
2013-10-26 23:02:02 +1100

    test for user input containg slash & colons

--- spec/lib/zendesk_apps_tools/settings_spec.rb
+++ spec/lib/zendesk_apps_tools/settings_spec.rb
@@ -11,6 +11,24 @@ describe ZendeskAppsTools::Settings do
   end
 
   describe '#get_settings_from' do
+    it 'accepts user input with colon & slashes' do
+      parameters = [
+        {
+          :name     => "required",
+          :required => true
+        }
+      ]
+
+      settings = {
+        "required" => "https://example.com:3000"
+      }
+
+      @user_input.stub(:ask).and_return('') # this represents the default user input
+      @user_input.stub(:ask).with("Enter a value for required parameter 'required':\n").and_return("https://example.com:3000")
+
+      result = @context.get_settings_from(@user_input, parameters).should == settings
+    end
+
     it 'prompts the user for settings' do
       parameters = [
         {
