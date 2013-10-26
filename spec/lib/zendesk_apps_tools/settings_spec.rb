58542952527bfe94f442cc43934aceeb1fd8fd9a
Bulkan Evcimen <bulkan@gmail.com>
2013-10-26 23:06:31 +1100

    setting default return value is common to all

--- spec/lib/zendesk_apps_tools/settings_spec.rb
+++ spec/lib/zendesk_apps_tools/settings_spec.rb
@@ -8,6 +8,7 @@ describe ZendeskAppsTools::Settings do
     @context = ZendeskAppsTools::Settings.new
     @user_input = Object.new
     @user_input.extend(ZendeskAppsTools::Common)
+    @user_input.stub(:ask).and_return('') # this represents the default user input
   end
 
   describe '#get_settings_from' do
@@ -23,7 +24,6 @@ describe ZendeskAppsTools::Settings do
         "required" => "https://example.com:3000"
       }
 
-      @user_input.stub(:ask).and_return('') # this represents the default user input
       @user_input.stub(:ask).with("Enter a value for required parameter 'required':\n").and_return("https://example.com:3000")
 
       result = @context.get_settings_from(@user_input, parameters).should == settings
@@ -59,7 +59,6 @@ describe ZendeskAppsTools::Settings do
         "not_required_with_default" => "789"
       }
 
-      @user_input.stub(:ask).and_return('') # this represents the default user input
       @user_input.stub(:ask).with("Enter a value for required parameter 'required':\n").and_return('xyz')
       @user_input.stub(:ask).with("Enter a value for optional parameter 'not_required' or press 'Return' to skip:\n").and_return('456')
 
