4bdd28fe2ab53b53d810e3d7c7a45bfb1a46fc5f
Po Chen <pchen@zendesk.com>
2014-01-02 17:45:38 +1100

    add command_spec with upload testing

---
+++ spec/lib/zendesk_apps_tools/command_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+require 'command'
+
+describe ZendeskAppsTools::Command do
+
+  before do
+    @command = ZendeskAppsTools::Command.new
+  end
+
+  it 'can be instantiated' do
+    @command.should_not == nil
+  end
+
+  describe '#upload' do
+
+    it 'returns the id of the successfully uploaded app zipfile' do
+      data = double('data')
+      Faraday::UploadIO.stub(:new) { data }
+
+      conn = double('conn')
+      @command.stub(:get_connection) { conn }
+
+      @command.stub(:package)
+
+      payload = { :uploaded_data => data }
+      response = double('response', :env => { :body => '{ "id": 123 }' })
+      conn.should_receive(:post).with('/api/v2/apps/uploads.json', payload).and_return(response)
+
+      @command.send(:upload, 'hell').should == 123
+    end
+
+  end
+
+end
