f12066a109cead468cf0c5b5a629d62046a122fc
Po Chen <pchen@zendesk.com>
2014-01-03 10:26:09 +1100

    add spec for update

--- spec/lib/zendesk_apps_tools/command_spec.rb
+++ spec/lib/zendesk_apps_tools/command_spec.rb
@@ -44,7 +44,7 @@ describe ZendeskAppsTools::Command do
       request = double('request')
       hash = double('hash')
       conn.should_receive(:post).and_yield(request)
-      request.should_receive(:url)
+      request.should_receive(:url).with('/api/v2/apps.json')
       request.should_receive(:headers).and_return(hash)
       hash.should_receive(:[]=).with(:content_type, 'application/json')
 
@@ -63,6 +63,56 @@ describe ZendeskAppsTools::Command do
 
       @command.should_receive(:say_error)
       expect { @command.create }.to_not raise_error(Faraday::Error::ClientError)
+    end
+
+  end
+
+  describe '#update' do
+
+    context 'when no errors occur' do
+      before do
+        @command.stub(:prepare_api_auth)
+        @command.stub(:upload) { 123 }
+
+        @app_id = 456
+
+        conn = double('conn')
+        @command.stub(:get_connection) { conn }
+
+        request = double('request')
+        hash = double('hash')
+        conn.should_receive(:put).and_yield(request)
+        request.should_receive(:url).with("/api/v2/apps/#{@app_id}.json")
+        request.should_receive(:headers).and_return(hash)
+        hash.should_receive(:[]=).with(:content_type, 'application/json')
+
+        body = JSON.generate :upload_id => '123'
+        request.should_receive(:body=).with(body)
+
+        @command.stub(:set_cache)
+        @command.stub(:check_status).and_return('ok', nil, nil)
+      end
+
+      it 'updates the app on server when app id is already in cache' do
+        @command.stub(:get_cache) { @app_id }
+
+        @command.update
+      end
+
+      it 'updates the app on server when app id is not already in cache' do
+        @command.stub(:get_cache) { nil }
+        @command.stub(:find_app_id) { @app_id }
+
+        @command.update
+      end
+    end
+
+    it 'gives helpful error message when things go wrong in faraday' do
+      @command.stub(:prepare_api_auth)
+      @command.stub(:upload).and_raise(Faraday::Error::ClientError.new('hey'))
+
+      @command.should_receive(:say_error)
+      expect { @command.update }.to_not raise_error(Faraday::Error::ClientError)
     end
 
   end
