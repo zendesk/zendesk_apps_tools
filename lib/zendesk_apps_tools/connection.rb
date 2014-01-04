95c0519b47b1702891bf539d15c856d62d15ce91
Po Chen <pchen@zendesk.com>
2014-01-04 17:35:57 +1100

    extract methods into separate files

---
+++ lib/zendesk_apps_tools/connection.rb
@@ -0,0 +1,36 @@
+module ZendeskAppsTools
+  module Connection
+    FULL_URL     = /https?:\/\//
+    URL_TEMPLATE = 'https://%s.zendesk.com/'
+
+    def prepare_api_auth
+      @subdomain ||= get_cache('subdomain') || get_value_from_stdin('Enter your Zendesk subdomain or full Zendesk URL:')
+      @username  ||= get_cache('username') || get_value_from_stdin('Enter your username:')
+
+      unless @password
+        print 'Enter your password: '
+        @password ||= STDIN.noecho(&:gets).chomp
+        puts
+
+        set_cache 'subdomain' => @subdomain, 'username' => @username
+      end
+    end
+
+    def get_full_url
+      if FULL_URL =~ @subdomain
+        @subdomain
+      else
+        URL_TEMPLATE % @subdomain
+      end
+    end
+
+    def get_connection(middleware = :url_encoded)
+      prepare_api_auth
+      Faraday.new get_full_url do |f|
+        f.request middleware
+        f.adapter :net_http
+        f.basic_auth @username, @password
+      end
+    end
+  end
+end
