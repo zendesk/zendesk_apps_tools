68f37e3525917324e98446e487b42e14812a974b
Po Chen <pchen@zendesk.com>
2014-01-04 17:58:41 +1100

    disable cache when zipfile is provided
    the user is probably not in the app directory

--- lib/zendesk_apps_tools/cache.rb
+++ lib/zendesk_apps_tools/cache.rb
@@ -3,6 +3,8 @@ module ZendeskAppsTools
     CACHE_FILE_NAME = '.zat'
 
     def set_cache(hash)
+      return if options[:zipfile]
+
       @cache = File.exists?(cache_path) ? JSON.parse(File.read(@cache_path)).update(hash) : hash
       File.open(@cache_path, 'w') { |f| f.write JSON.pretty_generate(@cache) }
     end
