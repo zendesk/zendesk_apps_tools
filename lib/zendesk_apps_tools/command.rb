b6c18fb30544cdecb76111e961e44246c5d9f6b7
Po Chen <pchen@zendesk.com>
2014-01-06 11:24:29 +1100

    extract method from package

--- lib/zendesk_apps_tools/command.rb
+++ lib/zendesk_apps_tools/command.rb
@@ -91,18 +91,7 @@ module ZendeskAppsTools
 
       archive_rel_path = relative_to_original_destination_root(archive_path)
 
-      Zip::ZipFile.open(archive_path, 'w') do |zipfile|
-        app_package.files.each do |file|
-          path = file.relative_path
-          say_status "package", "adding #{path}"
-
-          # resolve symlink to source path
-          if File.symlink? file.absolute_path
-            path = File.readlink(file.absolute_path)
-          end
-          zipfile.add(file.relative_path, app_dir.join(path).to_s)
-        end
-      end
+      zip archive_path
 
       say_status "package", "created at #{archive_rel_path}"
       true
@@ -200,6 +189,21 @@ module ZendeskAppsTools
       if ZendeskAppsSupport::AppVersion::CURRENT != version[:current]
         puts 'This tool is using an out of date Zendesk App Framework. Please upgrade!'
         exit 1
+      end
+    end
+
+    def zip(archive_path)
+      Zip::ZipFile.open(archive_path, 'w') do |zipfile|
+        app_package.files.each do |file|
+          path = file.relative_path
+          say_status "package", "adding #{path}"
+
+          # resolve symlink to source path
+          if File.symlink? file.absolute_path
+            path = File.readlink(file.absolute_path)
+          end
+          zipfile.add(file.relative_path, app_dir.join(path).to_s)
+        end
       end
     end
 
