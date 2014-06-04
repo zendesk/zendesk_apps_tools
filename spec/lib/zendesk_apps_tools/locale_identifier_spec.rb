c5bbd54a2d48f2b749fcb82636eaa628f036d19a
Likun Liu <lliu@zendesk.com>
2014-06-04 13:55:02 +1000

    use lowercase locale ids except for default locale 'en-US'

---
+++ spec/lib/zendesk_apps_tools/locale_identifier_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require 'locale_identifier'
+
+describe ZendeskAppsTools::LocaleIdentifier do
+
+  it 'should set locale id to en-US when code starts with en-US' do
+    ZendeskAppsTools::LocaleIdentifier.new('en-US-x-12').locale_id.should == 'en-US'
+  end
+
+  it 'should set locale id to zh-cn when code starts with zh-CN' do
+    ZendeskAppsTools::LocaleIdentifier.new('zh-CN-x-12').locale_id.should == 'zh-cn'
+  end
+end
