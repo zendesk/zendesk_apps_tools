d6a35d21be613be19b3c8d86f4297eb1579a6547
Po Chen <pchen@zendesk.com>
2015-02-18 16:25:12 +1100

    Remove deprecated setting, nothing is affected

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,6 @@ $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib')
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib/zendesk_apps_tools')
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
