8b6b01f753e740b5e07dc35a305eaae01afb1efa
Po Chen <pchen@zendesk.com>
2015-02-18 16:52:17 +1100

    Clean up spec

--- spec/lib/zendesk_apps_tools/bump.rb
+++ spec/lib/zendesk_apps_tools/bump.rb
@@ -14,46 +14,57 @@ describe ZendeskAppsTools::Bump do
   before do
     allow(subject).to receive(:load_manifest)
     allow(subject).to receive(:write_manifest)
-  end
-
-  it 'works with imcomplete semver version' do
-    subject.instance_variable_set(:@manifest, { 'version' => '1.0' })
-
-    VERSION_PARTS.each do |part|
-      expect { subject.send(part) }.not_to raise_error
-    end
+    subject.instance_variable_set(:@manifest, { 'version' => '1.2.3' })
   end
 
   describe '#major' do
-    before do
-      subject.instance_variable_set(:@manifest, { 'version' => '1.2.3' })
-      subject.major
-    end
+    before { subject.major }
 
-    it 'bumps major version, and cleans up minor and patch version' do
+    it 'bumps major version, and resets minor and patch version' do
       expect(version).to eq('2.0.0')
     end
   end
 
   describe '#minor' do
-    before do
-      subject.instance_variable_set(:@manifest, { 'version' => '1.2.3' })
-      subject.minor
-    end
+    before { subject.minor }
 
-    it 'bumps minor version, and cleans up patch version' do
+    it 'bumps minor version, and resets patch version' do
       expect(version).to eq('1.3.0')
     end
   end
 
   describe '#patch' do
-    before do
-      subject.instance_variable_set(:@manifest, { 'version' => '1.2.3' })
-      subject.patch
-    end
+    before { subject.patch }
 
     it 'bumps patch version' do
       expect(version).to eq('1.2.4')
+    end
+  end
+
+  context 'when version is not complete semver' do
+    before do
+      subject.instance_variable_set(:@manifest, { 'version' => '1.0' })
+    end
+
+    it 'works with incomplete semver version' do
+      VERSION_PARTS.each do |part|
+        expect { subject.send(part) }.not_to raise_error
+      end
+    end
+
+    it 'corrects the version to semver' do
+      subject.patch
+      expect(version).to eq('1.0.1')
+    end
+
+    it 'corrects the version to semver' do
+      subject.minor
+      expect(version).to eq('1.1.0')
+    end
+
+    it 'corrects the version to semver' do
+      subject.major
+      expect(version).to eq('2.0.0')
     end
   end
 end
