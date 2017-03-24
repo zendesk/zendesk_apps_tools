6174e8de96276a2e14a6304d11317fabf56f3db4
Roman Sandler <rsandler@zendesk.com>
2017-03-24 11:24:40 +1100

    add test

---
+++ features/create.feature
@@ -0,0 +1,12 @@
+Feature: upload an app to the apps marketplace
+  Background: create a new zendesk app
+
+  Scenario: package a zendesk app by running 'zat create --no-install' command
+    Given an app is created in directory "tmp/aruba"
+    Given a .zat file in "tmp/aruba"
+    When I run the command "zat create --path tmp/aruba --no-install" to create the app
+    And the command output should contain "validate  OK"
+    And the command output should contain "package  created"
+    And the command output should contain "Status  working"
+    And the command output should contain "Create  OK"
+    And the zip file should exist in directory "tmp/aruba/tmp"
--- features/step_definitions/app_steps.rb
+++ features/step_definitions/app_steps.rb
@@ -31,6 +31,12 @@ Given /^a(n|(?: v1)) app is created in directory "(.*?)"$/ do |version, app_dir|
     )
 end
 
+Given /^a \.zat file in "(.*?)"/ do |app_dir|
+  f = File.new(File.join(app_dir, '.zat'), 'w')
+  f.write(JSON.dump(username: 'test@user.com', password: 'hunter2', subdomain: 'app-account'))
+  f.close
+end
+
 When /^I run "(.*?)" command with the following details:$/ do |cmd, table|
   IO.popen(cmd, 'w+') do |pipe|
     # [ ['parameter name', 'value'] ]
@@ -49,8 +55,15 @@ When /^I create a symlink from "(.*?)" to "(.*?)"$/ do |src, dest|
   FileUtils.ln_s(src, dest)
 end
 
-When /^I run the command "(.*?)" to (validate|package|clean) the app$/ do |cmd, _action|
-  IO.popen(cmd, 'w+') do |pipe|
+When /^I run the command "(.*?)" to (validate|package|clean|create) the app$/ do |cmd, _action|
+  if cmd.start_with? 'zat create'
+    path = File.expand_path('../../support/webmock', __FILE__)
+    env_hash = { 'RUBYOPT' => "-r #{path}" }
+  else
+    env_hash = {}
+  end
+  cmd.sub!(/^zat/, File.expand_path('../../../bin/zat', __FILE__))
+  IO.popen(env_hash, cmd, 'w+') do |pipe|
     pipe.puts "\n"
     pipe.close_write
     @output = pipe.readlines
