32e0fd80fc1684667ff74ff7950314c5d0c21fa8
Keith <kchong@zendesk.com>
2018-12-02 21:49:56 +1100

    Mock Api Response with receive_message_chain

--- spec/lib/zendesk_apps_tools/deploy_spec.rb
+++ spec/lib/zendesk_apps_tools/deploy_spec.rb
@@ -3,31 +3,22 @@ require 'zendesk_apps_tools/deploy'
 require 'json'
 
 describe ZendeskAppsTools::Deploy do
-  let(:mock_response) do
-    Class.new do
-      attr_reader :body
-      def initialize(json_body)
-        @body = json_body
-      end
-
-      def get(uri)
-        self
-      end
-    end
+  let(:api_response) do
+   {
+     'apps'=> [
+        { 'name'=> 'notification_app', 'id'=> 22 },
+        { 'name'=> 'time_tracking_app', 'id' => 99 }
+      ]
+    }.to_json
   end
 
   let(:subject_class) do
     Class.new do
       include ZendeskAppsTools::Deploy
-      attr_reader :get_connection, :app_name
+      attr_reader :app_name
 
-      def initialize(get_response, app_name)
-        @get_connection = get_response
+      def initialize(app_name)
         @app_name = app_name
-      end
-
-      def connection(multipart = nil)
-        get_connection
       end
 
       def get_value_from_stdin(text)
@@ -38,23 +29,11 @@ describe ZendeskAppsTools::Deploy do
 
   describe '#find_app_id' do
 
-    let(:api_response) do
-      mock_response.new(
-       {
-         'apps'=> [
-            { 'name'=> 'notification_app', 'id'=> 22 },
-            { 'name'=> 'time_tracking_app', 'id' => 99 }
-          ]
-        }.to_json
-      )
-    end
-
-    let(:subject) { subject_class.new(api_response) }
-
     context 'user inputs an app name that is NOT in api response' do
       it 'errors and exits system' do
-        subject = subject_class.new(api_response, 'random_app_name')
-        allow(subject).to receive_message_chain(:say_status, :command, :text)
+        subject = subject_class.new('random_app_name')
+        allow(subject).to receive(:say_status)
+        allow(subject).to receive_message_chain(:connection, :get, :body) { api_response }
 
         expect(subject).to receive(:say_error).with(
           "App not found. " \
@@ -65,18 +44,21 @@ describe ZendeskAppsTools::Deploy do
     end
 
     context 'user inputs an app name that is in api response' do
+
       it 'returns app id 22 for notification_app' do
-        subject = subject_class.new(api_response, 'notification_app')
+        subject = subject_class.new('notification_app')
         allow(subject).to receive_message_chain(:cache, :save)
-        allow(subject).to receive_message_chain(:say_status, :command, :text)
+        allow(subject).to receive(:say_status)
+        allow(subject).to receive_message_chain(:connection, :get, :body) { api_response }
 
         expect(subject.find_app_id).to eq(22)
       end
 
       it 'returns app id 99 for notification_app' do
-        subject = subject_class.new(api_response, 'time_tracking_app')
+        subject = subject_class.new('time_tracking_app')
         allow(subject).to receive_message_chain(:cache, :save)
-        allow(subject).to receive_message_chain(:say_status, :command, :text)
+        allow(subject).to receive(:say_status)
+        allow(subject).to receive_message_chain(:connection, :get, :body) { api_response }
 
         expect(subject.find_app_id).to eq(99)
       end
