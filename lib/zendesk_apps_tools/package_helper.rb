0c283f29d11027ee0e67c749b1d7db539dbc0cb5
Olaf Kwant <olaf+github@zendesk.com>
2019-05-02 14:27:03 +1000

    Warn user about manifest.json parsing errors

--- lib/zendesk_apps_tools/package_helper.rb
+++ lib/zendesk_apps_tools/package_helper.rb
@@ -9,6 +9,8 @@ module ZendeskAppsTools
     def manifest
       begin
         @manifest ||= app_package.manifest
+      rescue JSON::ParserError, ZendeskAppsSupport::Manifest::OverrideError => e
+        say_status "error", "Manifest file is incorrectly formatted: #{e.message}", :red and exit 1
       rescue Errno::ENOENT
         say_status "error", "Manifest file cannot be found in the given path. Check you are pointing to the path that contains your manifest.json", :red and exit 1
       end
