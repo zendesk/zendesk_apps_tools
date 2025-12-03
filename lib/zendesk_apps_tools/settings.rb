8b172339977d0b1b163002fb83a687db44499fbb
Subhasish Karmakar <subhasish.karmakar@zendesk.com>
2025-12-03 13:01:37 +0530

    Ruby version upgrade to 3.1.0

--- lib/zendesk_apps_tools/settings.rb
+++ lib/zendesk_apps_tools/settings.rb
@@ -81,7 +81,7 @@ module ZendeskAppsTools
           JSON.load(contents)
         else
           require 'yaml'
-          YAML.load(contents)
+          YAML.safe_load(contents)
         end
       settings_data.each do |index, setting|
         if setting.is_a?(Hash) || setting.is_a?(Array)
